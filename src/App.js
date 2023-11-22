e8c513e129be8ae8678c7b6dbe46a2e8cb8dd9b3
dev-bhoomi <bhoomigupta912536@gmail.com>
2023-11-22 13:22:20 +0530

    search

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './App.css';
+import { Users } from './users';
+import Table from './Table';
+
+const App = () => {
+  const [query,setQuery] = useState("");
+
+  const keys = ["first_name","last_name","email"]
+
+  const search=(data) =>{
+    return data.filter((item) => 
+   keys.some((key) => item[key].toLowerCase().includes(query))
+    
+    );
+  };
+  return (
+    <div className='app'>
+   <input 
+   type="text" 
+   placeholder='Search...' 
+   className='search'
+   onChange={(e) => setQuery(e.target.value)}
+   />
+   <Table data={search(Users)} />
+    </div>
+  )
+}
+
+export default App;
+
+
+
+
